917497811fc92ddf2f0f98c847745a8733e0ae45
luismariolem <luismariolem@gmail.com>
2023-06-12 12:43:51 -0600

    Add files via upload

---
+++ word-and-pdf-handling-main/src/main/java/dev/mamasoyjuanito/utils/Constantes.java
@@ -0,0 +1,8 @@
+package dev.mamasoyjuanito.utils;
+
+public class Constantes {
+
+	public static final String EXTENSION_DOCX = "docx";
+	public static final String EXTENSION_PDF = "pdf";
+	
+}
---
+++ word-and-pdf-handling-main/src/test/java/dev/mamasoyjuanito/handling/WordAndPdfHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mamasoyjuanito.handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordAndPdfHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
